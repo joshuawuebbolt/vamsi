e4cabb97028d83ff821c8de5cc011f761a2cf36a
Joshua Jude Wuebbolt <joshua.wuebbolt@mail.utoronto.ca>
2025-10-25 13:37:31 -0400

    Added daysSince function

--- extension.js
+++ extension.js
@@ -2,31 +2,34 @@
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "vamsi" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
+	// Hello World command
 	const disposable = vscode.commands.registerCommand('vamsi.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World from Vamsi!');
 	});
 
-	
+	// Days since last saw Vamsi (last seen: May 18, 2025)
+	const disposableDays = vscode.commands.registerCommand('vamsi.daysSince', function () {
+		const lastSeen = new Date(2025, 4, 10); // month is 0-indexed: 4 = May
+		const now = new Date();
+		const diffMs = now.getTime() - lastSeen.getTime();
+		const days = Math.floor(diffMs / (1000 * 60 * 60 * 24));
+		if (isNaN(days)) {
+			vscode.window.showInformationMessage('Could not compute days since May 18, 2025.');
+		} else if (days >= 0) {
+			vscode.window.showInformationMessage(`It's been ${days} day(s) since you saw Vamsi (${lastSeen}).`);
+		} else {
+			vscode.window.showInformationMessage(`May 18, 2025 is ${Math.abs(days)} day(s) from now.`);
+		}
+	});
 
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(disposable, disposableDays);
 }
 
 // This method is called when your extension is deactivated
